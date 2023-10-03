edcceca41e45df7ad8e8816713351bd54e4c77c8
Mino <minowong@gmail.com>
2023-10-03 09:58:10 -0700

    New comment committed

--- app/src/main/java/com/example/lec4githubdemo/MainActivity.java
+++ app/src/main/java/com/example/lec4githubdemo/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Made some changes locally
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
